2c46fc398cf0ef2e27030d064e54eb5f4c9eb7a2
Matthew Dargan <matthewdargan57@gmail.com>
2024-01-01 19:33:33 -0600

    scan: refactor lex logic
    Remove node.go and parse.go until lex implementation is more complete.
    Refactor parse package into scan package for lex logic. The parse
    package will be added later specifically for the parser.

---
+++ scan/scan.go
@@ -0,0 +1,294 @@
+// Copyright 2023 Matthew P. Dargan. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate stringer -type Type
+
+package scan
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+// Token represents a token or text string returned from the scanner.
+type Token struct {
+	Type Type   // The type of this item.
+	Line int    // The line number on which this token appears
+	Text string // The text of this item.
+}
+
+// Type identifies the type of lex items.
+type Type int
+
+const (
+	EOF             Type = iota
+	Error                // error occurred; value is text of error
+	BlankLine            // blank lines separate elements
+	Space                // run of spaces
+	Text                 // plain text
+	Comment              // comment marker
+	HyperlinkName        // hyperlink target name
+	HyperlinkPrefix      // prefix before hyperlink target name
+	HyperlinkStart       // indicates the start of a hyperlink target
+	HyperlinkSuffix      // suffix after hyperlink target name
+	HyperlinkURI         // URI a hyperlink target points to
+)
+
+func (i Token) String() string {
+	switch {
+	case i.Type == EOF:
+		return "EOF"
+	case i.Type == Error:
+		return "error: " + i.Text
+	case len(i.Text) > 10:
+		return fmt.Sprintf("%s: %.10q...", i.Type, i.Text)
+	}
+	return fmt.Sprintf("%s: %q", i.Type, i.Text)
+}
+
+const eof = -1
+
+// stateFn represents the state of the scanner as a function that returns the next state.
+type stateFn func(*Scanner) stateFn
+
+// Scanner holds the state of the scanner.
+type Scanner struct {
+	r         io.ByteReader
+	done      bool
+	name      string // the name of the input; used only for error reports
+	buf       []byte // I/O buffer, re-used.
+	input     string // the line of text being scanned.
+	lastRune  rune   // most recent return from next()
+	lastWidth int    // size of that rune
+	readOK    bool   // allow reading of a new line of input
+	line      int    // line number in input
+	pos       int    // current position in the input
+	start     int    // start position of this item
+	token     Token  // token to return to parser
+}
+
+// loadLine reads the next line of input and stores it in (appends it to) the input.
+// (l.input may have data left over when we are called.)
+// It strips carriage returns to make subsequent processing simpler.
+func (l *Scanner) loadLine() {
+	l.buf = l.buf[:0]
+	for {
+		c, err := l.r.ReadByte()
+		if err != nil {
+			l.done = true
+			break
+		}
+		if c != '\r' { // There will never be a \r in l.input.
+			l.buf = append(l.buf, c)
+		}
+		if c == '\n' {
+			break
+		}
+	}
+	// Reset to beginning of input buffer if there is nothing pending.
+	if l.start == l.pos {
+		l.input = string(l.buf)
+		l.start = 0
+		l.pos = 0
+	} else {
+		l.input += string(l.buf)
+	}
+}
+
+// readRune reads the next rune from the input.
+func (l *Scanner) readRune() (rune, int) {
+	if !l.done && l.pos == len(l.input) {
+		if !l.readOK { // Token did not end before newline.
+			l.errorf("incomplete token")
+			return '\n', 1
+		}
+		l.loadLine()
+	}
+	if len(l.input) == l.pos {
+		return eof, 0
+	}
+	return utf8.DecodeRuneInString(l.input[l.pos:])
+}
+
+// next returns the next rune in the input.
+func (l *Scanner) next() rune {
+	l.lastRune, l.lastWidth = l.readRune()
+	l.pos += l.lastWidth
+	return l.lastRune
+}
+
+// peek returns but does not consume the next rune in the input.
+func (l *Scanner) peek() rune {
+	r, _ := l.readRune()
+	return r
+}
+
+// peek2 returns the next two runes ahead, but does not consume anything.
+func (l *Scanner) peek2() (rune, rune) {
+	pos := l.pos
+	lastWidth := l.lastWidth
+	r1 := l.next()
+	r2 := l.next()
+	l.pos = pos
+	l.lastWidth = lastWidth
+	return r1, r2
+}
+
+// backup steps back one rune. Should only be called once per call of next.
+func (l *Scanner) backup() {
+	if l.lastRune == eof {
+		return
+	}
+	if l.pos == l.start {
+		l.errorf("internal error: backup at start of input")
+	}
+	if l.pos > l.start {
+		l.pos -= l.lastWidth
+	}
+}
+
+// emit passes an item back to the client.
+func (l *Scanner) emit(t Type) stateFn {
+	if t == BlankLine {
+		l.line++
+	}
+	text := l.input[l.start:l.pos]
+	l.token = Token{t, l.line, text}
+	l.start = l.pos
+	return nil
+}
+
+// ignore skips over the pending input before this point.
+// It tracks newlines in the ignored text, so use it only
+// for text that is skipped without calling l.next.
+func (l *Scanner) ignore() {
+	l.line += strings.Count(l.input[l.start:l.pos], "\n")
+	l.start = l.pos
+}
+
+// accept consumes the next rune if it's from the valid set.
+func (l *Scanner) accept(valid string) bool {
+	if strings.ContainsRune(valid, l.next()) {
+		return true
+	}
+	l.backup()
+	return false
+}
+
+// acceptRun consumes a run of runes from the valid set.
+func (l *Scanner) acceptRun(valid string) {
+	for strings.ContainsRune(valid, l.next()) {
+	}
+	l.backup()
+}
+
+// errorf returns an error token and empties the input.
+func (l *Scanner) errorf(format string, args ...interface{}) stateFn {
+	l.token = Token{Error, l.start, fmt.Sprintf(format, args...)}
+	l.start = 0
+	l.pos = 0
+	l.input = l.input[:0]
+	return nil
+}
+
+// New creates and returns a new scanner.
+func New(name string, r io.ByteReader) *Scanner {
+	l := &Scanner{
+		r:    r,
+		name: name,
+		line: 1,
+	}
+	return l
+}
+
+// Next returns the next token.
+func (l *Scanner) Next() Token {
+	l.readOK = true
+	l.lastRune = eof
+	l.lastWidth = 0
+	l.token = Token{EOF, l.pos, "EOF"}
+	state := lexAny
+	for {
+		state = state(l)
+		if state == nil {
+			return l.token
+		}
+	}
+}
+
+const hyperlinkMark = ".. _"
+
+// lexAny scans non-space items.
+func lexAny(l *Scanner) stateFn {
+	switch r := l.next(); {
+	case r == eof:
+		return nil
+	case r == '\n':
+		return l.emit(BlankLine)
+	case unicode.IsSpace(r):
+		return lexSpace
+	case l.input[:l.pos] == hyperlinkMark:
+		return l.emit(HyperlinkPrefix)
+	case strings.LastIndex(l.input[:l.pos], hyperlinkMark) == 0:
+		if r == ':' {
+			return lexEndOfLine(l, HyperlinkSuffix)
+		}
+		return lexHyperlinkName
+	case r == '.' && l.peek() == '.':
+		l.next()
+		if r1, r2 := l.peek2(); r1 == ' ' && r2 == '_' {
+			return l.emit(HyperlinkStart)
+		}
+		return lexEndOfLine(l, Comment)
+	default:
+		return lexText
+	}
+}
+
+// lexSpace scans a run of space characters.
+func lexSpace(l *Scanner) stateFn {
+	for unicode.IsSpace(l.peek()) {
+		l.next()
+	}
+	return l.emit(Space)
+}
+
+// lexText scans until a newline or EOF.
+func lexText(l *Scanner) stateFn {
+	for {
+		switch l.peek() {
+		case eof:
+			return l.emit(Text)
+		case '\n':
+			i := l.emit(Text)
+			l.pos++
+			l.ignore()
+			return i
+		default:
+			l.next()
+		}
+	}
+}
+
+// lexEndOfLine scans a lex item known to be present.
+// An end-of-line character is ignored if it is present.
+func lexEndOfLine(l *Scanner, typ Type) stateFn {
+	i := l.emit(typ)
+	if l.peek() == '\n' {
+		l.pos++
+		l.ignore()
+	}
+	return i
+}
+
+// lexHyperlinkName scans a hyperlink name. The hyperlink name is known to be present.
+func lexHyperlinkName(l *Scanner) stateFn {
+	for l.peek() != ':' {
+		l.next()
+	}
+	return l.emit(HyperlinkName)
+}
---
+++ scan/scan_test.go
@@ -0,0 +1,358 @@
+// Copyright 2023 Matthew P. Dargan. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package scan
+
+import (
+	"strings"
+	"testing"
+)
+
+type scanTest struct {
+	name  string
+	input string
+	items []Token
+}
+
+func mkItem(typ Type, text string) Token {
+	return Token{
+		Type: typ,
+		Text: text,
+	}
+}
+
+var (
+	tEOF             = mkItem(EOF, "EOF")
+	tBlankLine       = mkItem(BlankLine, "\n")
+	tSpace           = mkItem(Space, " ")
+	tSpace2          = mkItem(Space, "  ")
+	tSpace3          = mkItem(Space, "   ")
+	tComment         = mkItem(Comment, "..")
+	tHyperlinkStart  = mkItem(HyperlinkStart, "..")
+	tHyperlinkPrefix = mkItem(HyperlinkPrefix, "_")
+	tHyperlinkSuffix = mkItem(HyperlinkSuffix, ":")
+)
+
+var scanTests = []scanTest{
+	{"empty", "", []Token{tEOF}},
+	{"spaces", " \t\n", []Token{mkItem(Space, " \t\n"), tEOF}},
+	{"text", `now is the time`, []Token{mkItem(Text, "now is the time"), tEOF}},
+	// comments
+	{
+		"line comment",
+		`.. A comment
+
+Paragraph.
+`,
+		[]Token{tComment, tSpace, mkItem(Text, "A comment"), tBlankLine, mkItem(Text, "Paragraph."), tEOF},
+	},
+	{
+		"comment block",
+		`.. A comment
+   block.
+
+Paragraph.
+`,
+		[]Token{
+			tComment, tSpace, mkItem(Text, "A comment"), tSpace3, mkItem(Text, "block."),
+			tBlankLine, mkItem(Text, "Paragraph."), tEOF,
+		},
+	},
+	{
+		"multi-line comment block",
+		`..
+   A comment consisting of multiple lines
+   starting on the line after the
+   explicit markup start.
+`,
+		[]Token{
+			tComment, tSpace3, mkItem(Text, "A comment consisting of multiple lines"),
+			tSpace3, mkItem(Text, "starting on the line after the"),
+			tSpace3, mkItem(Text, "explicit markup start."), tEOF,
+		},
+	},
+	{
+		"2 line comments",
+		`.. A comment.
+.. Another.
+
+Paragraph.
+`,
+		[]Token{
+			tComment, tSpace, mkItem(Text, "A comment."),
+			tComment, tSpace, mkItem(Text, "Another."),
+			tBlankLine, mkItem(Text, "Paragraph."), tEOF,
+		},
+	},
+	{
+		"line comment, no blank line",
+		`.. A comment
+no blank line
+
+Paragraph.
+`,
+		[]Token{
+			tComment, tSpace, mkItem(Text, "A comment"), mkItem(Text, "no blank line"),
+			tBlankLine, mkItem(Text, "Paragraph."), tEOF,
+		},
+	},
+	{
+		"2 line comments, no blank line",
+		`.. A comment.
+.. Another.
+no blank line
+
+Paragraph.
+`,
+		[]Token{
+			tComment, tSpace, mkItem(Text, "A comment."),
+			tComment, tSpace, mkItem(Text, "Another."), mkItem(Text, "no blank line"),
+			tBlankLine, mkItem(Text, "Paragraph."), tEOF,
+		},
+	},
+	{
+		"line comment with directive",
+		`.. A comment::
+
+Paragraph.
+`,
+		[]Token{tComment, tSpace, mkItem(Text, "A comment::"), tBlankLine, mkItem(Text, "Paragraph."), tEOF},
+	},
+	{
+		"comment block with directive",
+		`..
+   comment::
+
+The extra newline before the comment text prevents
+the parser from recognizing a directive.
+`,
+		[]Token{
+			tComment, tSpace3, mkItem(Text, "comment::"), tBlankLine,
+			mkItem(Text, "The extra newline before the comment text prevents"),
+			mkItem(Text, "the parser from recognizing a directive."), tEOF,
+		},
+	},
+	{
+		"comment block with hyperlink target",
+		`..
+   _comment: http://example.org
+
+The extra newline before the comment text prevents
+the parser from recognizing a hyperlink target.
+`,
+		[]Token{
+			tComment, tSpace3, mkItem(Text, "_comment: http://example.org"), tBlankLine,
+			mkItem(Text, "The extra newline before the comment text prevents"),
+			mkItem(Text, "the parser from recognizing a hyperlink target."), tEOF,
+		},
+	},
+	{
+		"comment block with citation",
+		`..
+   [comment] Not a citation.
+
+The extra newline before the comment text prevents
+the parser from recognizing a citation.
+`,
+		[]Token{
+			tComment, tSpace3, mkItem(Text, "[comment] Not a citation."), tBlankLine,
+			mkItem(Text, "The extra newline before the comment text prevents"),
+			mkItem(Text, "the parser from recognizing a citation."), tEOF,
+		},
+	},
+	{
+		"comment block with substitution definition",
+		`..
+   |comment| image:: bogus.png
+
+The extra newline before the comment text prevents
+the parser from recognizing a substitution definition.
+`,
+		[]Token{
+			tComment, tSpace3, mkItem(Text, "|comment| image:: bogus.png"), tBlankLine,
+			mkItem(Text, "The extra newline before the comment text prevents"),
+			mkItem(Text, "the parser from recognizing a substitution definition."), tEOF,
+		},
+	},
+	{
+		"comment block and empty comment",
+		`.. Next is an empty comment, which serves to end this comment and
+   prevents the following block quote being swallowed up.
+
+..
+
+    A block quote.
+`,
+		[]Token{
+			tComment, tSpace, mkItem(Text, "Next is an empty comment, which serves to end this comment and"),
+			tSpace3, mkItem(Text, "prevents the following block quote being swallowed up."),
+			tBlankLine, tComment, tBlankLine, mkItem(Space, "    "),
+			mkItem(Text, "A block quote."), // TODO: Should be itemBlockQuote once implemented
+			tEOF,
+		},
+	},
+	{
+		"comment in definition lists",
+		`term 1
+  definition 1
+
+  .. a comment
+
+term 2
+  definition 2
+`,
+		[]Token{
+			mkItem(Text, "term 1"), // TODO: Should be itemDefinitionTerm once implemented
+			tSpace2, mkItem(Text, "definition 1"), tBlankLine,
+			tSpace2, tComment, tSpace, mkItem(Text, "a comment"), tBlankLine,
+			mkItem(Text, "term 2"), tSpace2, mkItem(Text, "definition 2"), tEOF,
+		},
+	},
+	{
+		"comment after definition lists",
+		`term 1
+  definition 1
+
+.. a comment
+
+term 2
+  definition 2
+`,
+		[]Token{
+			mkItem(Text, "term 1"), // TODO: Should be itemDefinitionTerm once implemented
+			tSpace2, mkItem(Text, "definition 1"), tBlankLine,
+			tComment, tSpace, mkItem(Text, "a comment"), tBlankLine,
+			mkItem(Text, "term 2"), tSpace2, mkItem(Text, "definition 2"), tEOF,
+		},
+	},
+	{
+		"comment between bullet paragraphs 2 and 3",
+		`+ bullet paragraph 1
+
+  bullet paragraph 2
+
+  .. comment between bullet paragraphs 2 and 3
+
+  bullet paragraph 3
+`,
+		[]Token{
+			mkItem(Text, "+ bullet paragraph 1"), // TODO: Should be itemBullet once implemented
+			tBlankLine, tSpace2, mkItem(Text, "bullet paragraph 2"), tBlankLine,
+			tSpace2, tComment, tSpace, mkItem(Text, "comment between bullet paragraphs 2 and 3"),
+			tBlankLine, tSpace2, mkItem(Text, "bullet paragraph 3"), tEOF,
+		},
+	},
+	{
+		"comment between bullet paragraphs 1 and 2",
+		`+ bullet paragraph 1
+
+  .. comment between bullet paragraphs 1 (leader) and 2
+
+  bullet paragraph 2
+`,
+		[]Token{
+			mkItem(Text, "+ bullet paragraph 1"), // TODO: Should be itemBullet once implemented
+			tBlankLine, tSpace2,
+			tComment, tSpace, mkItem(Text, "comment between bullet paragraphs 1 (leader) and 2"),
+			tBlankLine, tSpace2, mkItem(Text, "bullet paragraph 2"), tEOF,
+		},
+	},
+	{
+		"comment trailing bullet paragraph",
+		`+ bullet
+
+  .. trailing comment
+`,
+		[]Token{
+			mkItem(Text, "+ bullet"), // TODO: Should be itemBullet once implemented
+			tBlankLine, tSpace2, tComment, tSpace, mkItem(Text, "trailing comment"), tEOF,
+		},
+	},
+	// hyperlink targets
+	{
+		"hyperlink target",
+		`.. _target:
+
+(Internal hyperlink target.)
+`,
+		[]Token{
+			tHyperlinkStart, tSpace, tHyperlinkPrefix,
+			mkItem(HyperlinkName, "target"), tHyperlinkSuffix,
+			tBlankLine, mkItem(Text, "(Internal hyperlink target.)"), tEOF,
+		},
+	},
+	{
+		"hyperlink target with optional space before colon", ".. _optional space before colon :",
+		[]Token{
+			tHyperlinkStart, tSpace, tHyperlinkPrefix, mkItem(HyperlinkName, "optional space before colon "),
+			tHyperlinkSuffix, tEOF,
+		},
+	},
+	{
+		"external hyperlink targets",
+		`External hyperlink targets:
+
+.. _one-liner: http://structuredtext.sourceforge.net
+
+.. _starts-on-this-line: http://
+                         structuredtext.
+                         sourceforge.net
+
+.. _entirely-below:
+   http://structuredtext.
+   sourceforge.net
+
+.. _escaped-whitespace: http://example.org/a\ path\ with\
+   spaces.html
+
+.. _not-indirect: uri\_
+`,
+		[]Token{
+			mkItem(Text, "External hyperlink targets:"), tBlankLine,
+			tHyperlinkStart, tSpace, tHyperlinkPrefix, mkItem(HyperlinkName, "one-liner"), tHyperlinkSuffix,
+			tSpace, // TODO: hyperlink URI
+			tEOF,
+		},
+	},
+}
+
+// collect gathers the emitted items into a slice.
+func collect(t *scanTest) (items []Token) {
+	s := New(t.name, strings.NewReader(t.input))
+	for {
+		i := s.Next()
+		items = append(items, i)
+		if i.Type == EOF || i.Type == Error {
+			break
+		}
+	}
+	return
+}
+
+func equal(i1, i2 []Token, checkPos bool) bool {
+	if len(i1) != len(i2) {
+		return false
+	}
+	for k := range i1 {
+		if i1[k].Type != i2[k].Type {
+			return false
+		}
+		if i1[k].Text != i2[k].Text {
+			return false
+		}
+		if checkPos && i1[k].Line != i2[k].Line {
+			return false
+		}
+	}
+	return true
+}
+
+func TestScan(t *testing.T) {
+	for _, test := range scanTests {
+		items := collect(&test)
+		if !equal(items, test.items, false) {
+			t.Fatalf("%s: got\n\t%+v\nexpected\n\t%v", test.name, items, test.items)
+		}
+	}
+}
